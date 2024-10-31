84a8372ae45fc54bd913910875790aadd3cdb06b
Nejc Schneider <nejc.sch@gmail.com>
2024-10-31 21:16:23 +0100

    metadata-pointer: add updateMetadataField.js

---
+++ metadata-pointer/scripts/updateMetadataField.js
@@ -0,0 +1,21 @@
+// updateMetadataField.js
+const { createUpdateFieldInstruction } = require("@solana/spl-token-metadata");
+const { connection, payer } = require("./initializeConnection");
+
+async function updateMetadataField(mint, updateAuthority, field, value) {
+  const transaction = new Transaction().add(
+    createUpdateFieldInstruction({
+      programId: TOKEN_2022_PROGRAM_ID,
+      metadata: mint,
+      updateAuthority: updateAuthority,
+      field: field,
+      value: value,
+    })
+  );
+
+  const transactionSignature = await connection.sendTransaction(transaction, [payer]);
+
+  console.log(`\nMetadata Field Updated: https://solana.fm/tx/${transactionSignature}?cluster=devnet-solana`);
+}
+
+module.exports = { updateMetadataField };
