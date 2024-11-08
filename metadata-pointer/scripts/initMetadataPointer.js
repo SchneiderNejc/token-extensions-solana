a75a2be09431e31accf65f7cd76726ad7b1300aa
Nejc Schneider <nejc.sch@gmail.com>
2024-11-08 15:13:32 +0100

    initMetadataPointer.js: Convert mint and updateAuthority to PublicKey

--- metadata-pointer/scripts/initMetadataPointer.js
+++ metadata-pointer/scripts/initMetadataPointer.js
@@ -1,8 +1,12 @@
-const { createInitializeMetadataPointerInstruction } = require("@solana/spl-token");
-const { sendAndConfirmTransaction, Transaction } = require("@solana/web3.js");
+const { createInitializeMetadataPointerInstruction, TOKEN_2022_PROGRAM_ID } = require("@solana/spl-token");
+const { sendAndConfirmTransaction, Transaction, PublicKey } = require("@solana/web3.js");
 const { connection, payer } = require("./initConnection");
 
 async function initializeMetadataPointer(mint, updateAuthority) {
+  // Convert mint and updateAuthority to PublicKey if they are not already
+  const mint = new PublicKey(mint);
+  const updateAuthority = new PublicKey(updateAuthority);
+
   const transaction = new Transaction().add(
     createInitializeMetadataPointerInstruction(
       mint,
