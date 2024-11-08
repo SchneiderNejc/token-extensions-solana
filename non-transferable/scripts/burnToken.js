568cb11439bd49763abfa71f562ecf2ca460e709
Nejc Schneider <nejc.sch@gmail.com>
2024-11-08 21:00:19 +0100

    non-transferable: add burnToekn.js

---
+++ non-transferable/scripts/burnToken.js
@@ -0,0 +1,43 @@
+const { burn, TOKEN_2022_PROGRAM_ID } = require("@solana/spl-token");
+const {
+  clusterApiUrl,
+  Connection,
+  Keypair,
+  PublicKey,
+} = require("@solana/web3.js");
+const os = require("os");
+const fs = require("fs");
+
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+const secretKey = JSON.parse(
+  fs.readFileSync(`${os.homedir()}/.config/solana/id.json`, "utf8")
+);
+const payer = Keypair.fromSecretKey(new Uint8Array(secretKey));
+
+// @note Update mint address.
+const mint = new PublicKey("JAVPtYcq2RzGNztFw4jbA69gVLh4Kve1592xMGTCQa1A");
+const tokenAccount = new PublicKey(
+  "FC7bSaXhuy3sLgk5jtfd4yQi4EXjQYeEZM6W5giVyyY9"
+);
+
+(async () => {
+  // Burn tokens
+  const transactionSignature = await burn(
+    connection,
+    payer, // Transaction fee payer
+    tokenAccount, // Burn from
+    mint, // Mint Account address
+    payer.publicKey, // Token Account owner
+    100, // Amount
+    undefined, // Additional signers
+    undefined, // Confirmation options
+    TOKEN_2022_PROGRAM_ID // Token Extension Program ID
+  );
+
+  console.log("\nToken Burned:", tokenAccount.toBase58());
+
+  console.log(
+    "\nBurn Tokens:",
+    `https://solana.fm/tx/${transactionSignature}?cluster=devnet-solana`
+  );
+})();
