def976d72aea109d34122b62f335eab421313c0c
Nejc Schneider <nejc.sch@gmail.com>
2024-11-11 20:42:11 +0100

    add index.js

---
+++ reallocate/index.js
@@ -0,0 +1,80 @@
+const {
+  createReallocateInstruction,
+  createEnableRequiredMemoTransfersInstruction,
+  ExtensionType,
+  TOKEN_2022_PROGRAM_ID,
+  createMint,
+  createAccount,
+} = require("@solana/spl-token");
+const {
+  clusterApiUrl,
+  sendAndConfirmTransaction,
+  Connection,
+  Keypair,
+  Transaction,
+} = require("@solana/web3.js");
+const os = require("os");
+const fs = require("fs");
+
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+const secretKey = JSON.parse(
+  fs.readFileSync(`${os.homedir()}/.config/solana/id.json`, "utf8")
+);
+const payer = Keypair.fromSecretKey(new Uint8Array(secretKey));
+const mintAuthority = payer;
+const decimals = 9;
+
+(async () => {
+  // Create mint.
+  const mint = await createMint(
+    connection,
+    payer,
+    mintAuthority.publicKey,
+    mintAuthority.publicKey,
+    decimals,
+    undefined,
+    undefined,
+    TOKEN_2022_PROGRAM_ID
+  );
+
+  // Create TA.
+  const owner = Keypair.generate();
+  const account = await createAccount(
+    connection,
+    payer,
+    mint,
+    owner.publicKey,
+    undefined,
+    undefined,
+    TOKEN_2022_PROGRAM_ID
+  );
+
+  // Reallocate, enable memo transfer extension.
+  const extensions = [ExtensionType.MemoTransfer];
+  const transaction = new Transaction().add(
+    createReallocateInstruction(
+      account,
+      payer.publicKey,
+      extensions,
+      owner.publicKey,
+      undefined,
+      TOKEN_2022_PROGRAM_ID
+    ),
+    createEnableRequiredMemoTransfersInstruction(
+      account,
+      owner.publicKey,
+      [],
+      TOKEN_2022_PROGRAM_ID
+    )
+  );
+  const transactionSignature = await sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [payer, owner],
+    undefined
+  );
+
+  console.log(
+    `Transaction URL: https://solana.fm/tx/${transactionSignature}?cluster=devnet-solana`
+  );
+})();
