6df1e98cba7668761027f498e5beb22e30ea3f80
Nejc Schneider <nejc.sch@gmail.com>
2024-11-14 10:53:44 +0100

    mintTokens.js: load mint address from .json file

--- transfer-fees/mintTokens.js
+++ transfer-fees/mintTokens.js
@@ -11,24 +11,43 @@ const {
 } = require("@solana/spl-token");
 const os = require("os");
 const fs = require("fs");
+const path = require("path");
 
 // Setup connection & connect wallet.
 const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 const secretKey = JSON.parse(
   fs.readFileSync(`${os.homedir()}/.config/solana/id.json`, "utf8")
 );
-
-// Mint params.
-const mint = new PublicKey("FRyKjBBLnru1WAThr5bM7UfZygkP25y4u9iVtAwHqZX8");
 const payer = Keypair.fromSecretKey(new Uint8Array(secretKey));
 const mintAmount = BigInt(1_000_000_000);
 const mintAuthority = payer;
 
+// Define the path for accounts.json
+const dataPath = path.join(__dirname, "accounts.json");
+
+// Load mint address from accounts.json or log error if it doesn't exist
+let mint;
+if (fs.existsSync(dataPath)) {
+  const accountData = JSON.parse(fs.readFileSync(dataPath, "utf8"));
+  if (accountData.mintAddress) {
+    mint = new PublicKey(accountData.mintAddress);
+  } else {
+    console.error("Mint account not found. Run createMint.js first.");
+    process.exit(1);
+  }
+} else {
+  console.error("accounts.json file not found. Run createMint.js first.");
+  process.exit(1);
+}
+
+// Main function.
 (async () => {
   // Get sender's TA.
   const senderTA = await getAssociatedTokenAddress(
     mint, // Mint address
-    payer.publicKey // Owner's public key
+    payer.publicKey, // Owner's public key
+    false,
+    TOKEN_2022_PROGRAM_ID
   );
 
   const existingSenderTA = await connection.getAccountInfo(senderTA);
